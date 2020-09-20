7765bb870fe7ff1f6ce212a2e5e2af461b074055
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-09-20 19:51:53 +0600

    scrape individual news

--- ProthomAloScrapper.py
+++ ProthomAloScrapper.py
@@ -8,19 +8,29 @@ stories_container = driver.find_element_by_class_name(
     "searchStories1AdWithLoadMore-m__stories__2SFip"
 )
 
-links = stories_container.find_elements_by_tag_name("a")
+story_cards = stories_container.find_elements_by_class_name(
+    "customStoryCard9-m__wrapper__yEFJV"
+)
 load_more_button = driver.find_element_by_class_name("more-m__bn-content__3Ppnx")
 
+links = [
+    card.find_element_by_tag_name("a").get_attribute("href") for card in story_cards
+]
+
+
 for link in links:
-    print(link.get_attribute("href"))
+    driver.get(link)
+    time.sleep(3)
+    heading = driver.find_element_by_tag_name("h1")
+    print(heading.text)
+    p = driver.find_elements_by_tag_name("p")
+    for _p in p:
+        print(_p.text)
+    print("---------------")
 
-print("Waiting..")
-time.sleep(5)
-load_more_button.click()
-print("Clicked")
+# print("Waiting..")
+# time.sleep(5)
+# load_more_button.click()
+# print("Clicked")
 
-print("Waiting..")
-time.sleep(5)
-load_more_button.click()
-print("Clicked")
 # driver.close()
