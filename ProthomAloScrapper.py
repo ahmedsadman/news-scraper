cf776af2f19d156fb38be4a98aca8764cc1fe99d
Ahmed Sadman <ahmedsadman.211@gmail.com>
2020-09-21 00:45:58 +0600

    write output to csv

--- ProthomAloScrapper.py
+++ ProthomAloScrapper.py
@@ -1,4 +1,5 @@
 import time
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,6 +11,9 @@ class ProthomAloScrapper:
         self.BASE_URL = "https://www.prothomalo.com/search"
         self.driver = webdriver.Chrome()
         self.news_links = []
+        self.csv_file = open("output.csv", "w", encoding="utf-8", newline="\n")
+        self.csv_writer = csv.writer(self.csv_file, delimiter=",")
+        self.csv_writer.writerow(["title", "content", "tags"])
 
     def scroll_to_element(self, browser, element):
         self.driver.execute_script("arguments[0].scrollIntoView();", element)
@@ -43,19 +47,17 @@ class ProthomAloScrapper:
         """
         ad_banner_removed = False
         while iterations > 0:
-            print("waiting...")
             load_button = self.driver.find_element_by_class_name(
                 "more-m__wrapper__2sxTa"
             )
             self.scroll_to_element(self.driver, load_button)
 
             if not ad_banner_removed:
-                print("trying to remove ad banner")
+                print("Trying to remove ad banner...")
                 time.sleep(3)
                 ad_banner_removed = self.remove_ad_banner()
 
             load_button.click()
-            print("clicked")
             iterations -= 1
             time.sleep(3)
 
@@ -66,7 +68,8 @@ class ProthomAloScrapper:
         self.driver.get(self.BASE_URL)
 
         # load all links before starting to process
-        self.load_more(5)
+        print("Getting news article links...")
+        self.load_more(30)
 
         stories_container = self.driver.find_element_by_class_name(
             "searchStories1AdWithLoadMore-m__stories__2SFip"
@@ -83,32 +86,45 @@ class ProthomAloScrapper:
 
     def scrape_article(self, browser, url, wait_time=5):
         try:
+            # avoid only-video type articles
+            if "video/" in url:
+                return
+
+            print(f"Scraping url: {url}")
             browser.get(url)
-            # time.sleep(wait_time)
             WebDriverWait(browser, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, "h1"))
             )
             WebDriverWait(browser, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, "p"))
             )
-            heading = browser.find_element_by_tag_name("h1")
-            print(heading.text)
 
+            heading = browser.find_element_by_tag_name("h1").text.strip()
             paras = browser.find_elements_by_tag_name("p")
-            paras = [para.text for para in paras]
-            for para in paras:
-                print(para)
-            print("---------------")
+            paras = [para.text.replace("\n", "").replace("\r", "") for para in paras]
+            paras = " ".join(paras)
+
+            # get the tags
+            tags = browser.find_elements_by_class_name("storyTag-m__story-tag__uEX9i")
+            tags = [tag.text.strip() for tag in tags]
+            tags = ",".join(tags)
+
+            # write output to csv
+            self.csv_writer.writerow([heading, paras, tags])
+
         except Exception as e:
-            pass
+            print("exception on article csv write", str(e))
 
     def batch_scrape(self):
+        print("Starting batch scraping...")
         self.get_news_links()
         for link in self.news_links:
             self.scrape_article(self.driver, link)
+        print(" -- COMPLETE --")
 
     def __del__(self):
         self.driver.close()
+        self.csv_file.close()
 
 
 if __name__ == "__main__":
